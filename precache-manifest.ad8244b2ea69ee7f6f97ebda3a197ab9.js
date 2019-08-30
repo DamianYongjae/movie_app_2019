d53e8f78521e359bccb70d6abb338e29cc6f52b7
DamianYongjae <klick214@gmail.com>
2019-08-29 22:10:05 -0400

    Updates

---
+++ precache-manifest.ad8244b2ea69ee7f6f97ebda3a197ab9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6c22335f67af2077a9a9d260d29c60",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "cb850a1776821ec06eae",
+    "url": "/movie_app_2019/static/css/main.ff280ef7.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "cb850a1776821ec06eae",
+    "url": "/movie_app_2019/static/js/main.24d34a86.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
